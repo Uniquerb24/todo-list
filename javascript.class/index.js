60fe2d15ee5db3570ffaa4afa7b5ade3ce614f2c
esther lawrence <107858570+Uniquerb24@users.noreply.github.com>
2022-08-05 21:48:03 +0100

    Add files via upload

---
+++ javascript.class/index.js
@@ -0,0 +1,36 @@
+// alert('hello world')
+// assigning data type of string to variable
+const myName = "esther";
+
+// console.log(myName)
+
+// myName = 'vic'
+// console.log(myName)
+// assigning data type of number to variable
+
+// const myAge = 38;
+// console.log(myAge + 20);
+
+//array data type
+const ages = [20, 40, 16, 31, 20, 22, "esther"];
+const profile = [["esther"], ["food", "food", "more food", "movies"]];
+// accessing multi dimensional arrays
+const hobby = profile[1][3];
+console.log(hobby);
+console.log(ages);
+
+//trying to get the value of an item in the array
+// const nameInArray = ages[6];
+// console.log(nameInArray);
+const nameInArray = ages[2];
+console.log(nameInArray);
+
+//data type of objects
+
+//  const esthersInfo = {
+//     name: 'esther',
+//     age: 22,
+//     hands: 2,
+//     legs: 15,
+//     proffesions: ['estision', 'lash extension', 'micro blading', 'developer']
+// }
---
+++ javascript.class/index2.js
@@ -0,0 +1,19 @@
+// const shopping = [
+//   ["mango", 4, "apple", 5, "carrot", 3],
+//   ["rice", 4, "beans", 3, "garri", 4],
+//   ["indomie", 4, "sardine", 8, "biscuit", 4],
+//   ["choco", 4, "milk", 4, "sugar", 5],
+//   ["book", 3, "pen", 4, "pencil", 5],
+// ];
+// console.log(shopping);
+// const fruit = shopping[0][3];
+// console.log(fruit);
+// const foodstuff = shopping[1][5];
+// console.log(foodstuff);
+// const groceries = shopping[2][3];
+// console.log(groceries);
+// const provision = shopping[3][1];
+// console.log(provision);
+// const education = shopping[4][3];
+// console.log(education);
+
